d1f495bb96b0e617d92da44004f7a66cf76683e7
Subashini20 <78964674+Subashini20@users.noreply.github.com>
2022-01-07 21:19:04 +0530

    Update logincomponent.js

--- src/components/logincomponent.js
+++ src/components/logincomponent.js
@@ -56,7 +56,7 @@ export default class Login extends Component {
             <>
             <div className='signin'>
             <div className='App'>
-              <h1 style={{ padding: "10px", textAlign: "center",color: 'white', textShadow: ' 2px 2px #000000'  }}><b>Enhance your Knowledge=>E-books library </b></h1>
+              <h1 style={{ padding: "10px", textAlign: "center",color: 'white', textShadow: ' 2px 2px #000000'  }}><b><em>Enhance your Knowledge!!!E-books library</em> </b></h1>
               <pre><center><marquee behavior="scroll" direction="right"><b style={{fontSize:"20px",color:"white"}}><i>“Happy learning! Have a great day”</i></b></marquee></center></pre> </div>
          
             <div className="App" style={{padding:"5%"}}>
