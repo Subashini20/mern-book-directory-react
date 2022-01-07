7e8732ccd71119de6e25b075ec6de6aae1214105
Subashini20 <78964674+Subashini20@users.noreply.github.com>
2022-01-07 19:39:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./components/logincomponent";
 import SignUp from "./components/signupcomponent";
-import Home from "./components/Homecomponent";
 import Book from "./components/BookComponent";
 import BookAdmin from "./components/BookAdminComponent";
 import AddBook from "./components/AddBookComponent";
@@ -17,8 +16,8 @@ function App() {
         <div>
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/sign-in" component={Login} />
+    
+            <Route exact path="/" component={Login} />
             <Route exact path="/sign-up" component={SignUp} />
             <Route exact path="/book" component={Book}/>
             <Route exact path="/bookAdmin" component={BookAdmin}/>
