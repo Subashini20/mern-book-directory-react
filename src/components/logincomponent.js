1275dccb7d04d28e167ef995c6b7eab0244b83ad
Subashini20 <78964674+Subashini20@users.noreply.github.com>
2022-01-07 19:49:04 +0530

    Update logincomponent.js

--- src/components/logincomponent.js
+++ src/components/logincomponent.js
@@ -53,10 +53,12 @@ export default class Login extends Component {
          }
         }
         return (
+            <>
+            <div>
             <div className='App'>
-        <h1 style={{ padding: "20px", textAlign: "center",color: 'purple', textShadow: ' 2px 2px #414345'  }}><b>Welcome to E-books library </b></h1>
-        <pre><center><marquee behavior="scroll" direction="right"><b><i>“Happy learning! Have a great day”</i></b></marquee></center></pre>
-        </div>
+              <h1 style={{ padding: "20px", textAlign: "center",color: 'purple', textShadow: ' 2px 2px #414345'  }}><b>Welcome to E-books library </b></h1>
+              <pre><center><marquee behavior="scroll" direction="right"><b><i>“Happy learning! Have a great day”</i></b></marquee></center></pre> </div>
+            </div>
             <div className="App" style={{padding:"10%"}}>
                 <div className="auth-wrapper">
                     <div className="auth-inner">
@@ -96,6 +98,7 @@ export default class Login extends Component {
             </div>
             </div>
             </div>
+        </>
         );
     }
 }
